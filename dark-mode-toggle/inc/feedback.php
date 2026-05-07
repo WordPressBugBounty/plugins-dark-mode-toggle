7af87d204ef80266a56e677a336f972d443681cc
Sean Murphy <sean@iamseanmurphy.com>
2026-05-07 15:07:50 +0000

    1.7.2

--- dark-mode-toggle/inc/feedback.php
+++ dark-mode-toggle/inc/feedback.php
@@ -9,15 +9,15 @@ defined( 'ABSPATH' ) || die();
 ?>
 
 <div class="dmt-mt-4 xl:dmt-mt-6 dmt-px-1">
-	<a target="_blank" href="https://wordpress.org/support/plugin/dark-mode-toggle/reviews/#new-post" class="dmt-inline-block"><?php esc_html_e( 'Like this plugin? Leave a Review.', 'dark-mode-toggle' ); ?></a>
+	<a target="_blank" rel="noopener noreferrer" href="https://wordpress.org/support/plugin/dark-mode-toggle/reviews/#new-post" class="focus-visible:dmt-ring-2 dmt-inline-block"><?php esc_html_e( 'Like this plugin? Leave a Review.', 'dark-mode-toggle' ); ?></a>
 	<br>
-	<a target="_blank" href="https://wordpress.org/support/plugin/dark-mode-toggle/reviews/#new-post" class="dmt-inline-block dmt-mt-2">
-		<div class="dmt-rating dmt-pointer-events-none">
-			<input type="radio" class="dmt-mask dmt-mask-star-2 dmt-bg-orange-400">
-			<input type="radio" class="dmt-mask dmt-mask-star-2 dmt-bg-orange-400">
-			<input type="radio" class="dmt-mask dmt-mask-star-2 dmt-bg-orange-400">
-			<input type="radio" class="dmt-mask dmt-mask-star-2 dmt-bg-orange-400">
-			<input type="radio" class="dmt-mask dmt-mask-star-2 dmt-bg-orange-400">
+	<a target="_blank" rel="noopener noreferrer" href="https://wordpress.org/support/plugin/dark-mode-toggle/reviews/#new-post" class="focus-visible:dmt-ring-2 dmt-inline-block dmt-mt-2" aria-label="<?php esc_attr_e( 'Leave a review for this plugin.', 'dark-mode-toggle' ); ?>">
+		<div class="dmt-rating dmt-pointer-events-none" aria-hidden="true">
+			<input type="radio" tabindex="-1" class="dmt-mask dmt-mask-star-2 dmt-bg-orange-400">
+			<input type="radio" tabindex="-1" class="dmt-mask dmt-mask-star-2 dmt-bg-orange-400">
+			<input type="radio" tabindex="-1" class="dmt-mask dmt-mask-star-2 dmt-bg-orange-400">
+			<input type="radio" tabindex="-1" class="dmt-mask dmt-mask-star-2 dmt-bg-orange-400">
+			<input type="radio" tabindex="-1" class="dmt-mask dmt-mask-star-2 dmt-bg-orange-400">
 		</div>
 	</a>
 </div>
